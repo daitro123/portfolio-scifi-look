6dea5ad4cfd0f578ff35b7362c7faf8bb515c7ae
Tomas Macek <macek.tom@gmail.com>
2021-03-18 10:09:03 +0100

    responsivness completed

--- index.js
+++ index.js
@@ -1,40 +1,69 @@
 gsap.registerPlugin(ScrollTrigger, ScrollToPlugin, MotionPathPlugin);
 
-const initialTimeline = gsap.timeline({ ease: "power4.out" });
+// parallax on portfolio cards
+const animations = () => {
+	if (window.innerWidth > 600) {
+		const initialTimeline = gsap.timeline({ ease: "power4.out" });
+		initialTimeline
+			.to(".circle--blue", { opacity: 1, xPercent: -30, duration: 2.5 })
+			.to(".circle--pinkish", { opacity: 1, xPercent: 30, duration: 2 }, "-=2")
+			.from(".hero__heading", { opacity: 0, x: -20, duration: 1 }, "-=1")
+			.from(
+				".hero__subheading div",
+				{ opacity: 0, x: 15, duration: 1, ease: "Power1.out", stagger: 0.1 },
+				"-=0.6"
+			);
+	} else {
+		console.log("else");
+		const initialTimeline = gsap.timeline({ ease: "power4.out" });
+		initialTimeline
+			.to(".circle--blue", { opacity: 1, x: 0, duration: 1.5 })
+			.to(".circle--pinkish", { opacity: 1, x: 0, duration: 1 }, "-=0.5")
+			.from(".hero__heading", { opacity: 0, y: -10, duration: 0.8 }, "-=0.5")
+			.from(".hero__subheading div", { opacity: 0, y: 10, duration: 0.5 }, "-=0.3");
+	}
+};
 
-initialTimeline
-	.to(".circle--blue", { opacity: 1, xPercent: -30, duration: 2.5 })
-	.to(".circle--pinkish", { opacity: 1, xPercent: 30, duration: 2 }, "-=2")
-	.from(".hero__heading", { opacity: 0, x: -20, duration: 1 }, "-=1")
-	.from(
-		".hero__subheading div",
-		{ opacity: 0, x: 15, duration: 1, ease: "Power1.out", stagger: 0.1 },
-		"-=0.6"
+// clear inline styles on resize and set values to finished state (hero section circles)
+window.addEventListener("resize", () => {
+	gsap.set(
+		".portfolio__card, .portfolio__card img, .circle, .hero__heading, .hero__subheading div",
+		{ clearProps: "all" }
 	);
 
-// parallax on portfolio cards
+	if (window.innerWidth > 600) {
+		gsap.set(".circle--blue", { xPercent: -30, opacity: 1 });
+		gsap.set(".circle--pinkish", { xPercent: 30, opacity: 1 });
+	}
+});
 
-gsap.utils.toArray(".portfolio__card").forEach((element) => {
-	gsap.from(element.querySelector("img"), {
-		yPercent: 50,
-		ease: "none",
-		scrollTrigger: {
-			trigger: element,
-			start: "top bottom", // the default values
-			end: "center+=" + window.innerHeight / 4 + " center",
-			scrub: true,
-		},
-	});
-	gsap.from(element, {
-		yPercent: 50,
-		ease: "none",
-		scrollTrigger: {
-			trigger: element,
-			start: "top bottom", // the default values
-			end: "center center",
-			scrub: true,
-		},
-	});
+animations();
+
+ScrollTrigger.matchMedia({
+	"(min-width: 600px)": function () {
+		gsap.utils.toArray(".portfolio__card").forEach((element) => {
+			gsap.from(element.querySelector("img"), {
+				yPercent: 50,
+				ease: "none",
+				scrollTrigger: {
+					trigger: element,
+					start: "top bottom", // the default values
+					end: "center+=" + window.innerHeight / 4 + " center",
+					scrub: true,
+				},
+			});
+			gsap.from(element, {
+				yPercent: 50,
+				ease: "none",
+				scrollTrigger: {
+					trigger: element,
+					start: "top bottom", // the default values
+					end: "center center",
+					scrub: true,
+				},
+			});
+		});
+	},
 });
 
 gsap.from(".side-dot-nav", {
@@ -42,42 +71,6 @@ gsap.from(".side-dot-nav", {
 	scrollTrigger: {
 		trigger: ".portfolio",
 		start: "top center", // the default values
-		markers: true,
 		toggleActions: "play none none reverse",
 	},
-});
-
-//animate circles
-
-gsap.to(".circle--blueish", {
-	repeat: -1,
-	motionPath: {
-		path: [
-			{ x: 0, y: 0 },
-			{ x: 1200, y: -500 },
-			{ x: 400, y: 200 },
-		],
-		curviness: 2,
-	},
-	duration: 120,
-	rotate: 200,
-	yoyo: true,
-	ease: "power1.inOut",
-});
-
-gsap.to(".circle--greenish", {
-	repeat: -1,
-	motionPath: {
-		path: [
-			{ x: 0, y: 0 },
-			{ x: -1200, y: -200 },
-			{ x: -1400, y: 0 },
-			{ x: 100, y: 200 },
-		],
-		curviness: 2,
-	},
-	duration: 90,
-	rotate: 360,
-	yoyo: true,
-	ease: "power1.inOut",
 });
